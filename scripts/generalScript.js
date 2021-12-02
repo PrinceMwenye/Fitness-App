ee76fa8b50b0e236e56aa30f252416812aef11ad
Prince H Chabveka <pchabveka@my.bcit.ca>
2021-12-02 05:38:36 -0800

    Add recommended workout script

--- scripts/generalScript.js
+++ scripts/generalScript.js
@@ -111,3 +111,27 @@ function setWorkoutData(id) {
 read_display_beach_workout_one();
 read_display_beach_workout_two();
 read_display_beach_workout_three();
+
+
+
+// Get recommended beach workout (random)
+const recommended = db.collection("Reviews").where("Recommend", "==", "Yes")
+    .get()
+    .then(recom => {
+
+        size = recom.size;
+        recomendations = recom.docs;
+        var code = recomendations[0].data()['code'];
+        // console.log(code)
+
+        function read_recommended() {
+            db.collection("Workouts").doc("Beach workouts")
+                .onSnapshot(RecommendedDoc => {
+                    console.log(RecommendedDoc.data()[code])
+                    document.getElementById("recommended").src = RecommendedDoc.data()[code]
+                })
+        }
+
+        read_recommended()
+
+    })
