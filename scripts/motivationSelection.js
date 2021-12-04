575f6c4820b7c980cf8bf62b4be877b88d4ccdfe
OceNick <ozcenick@gmail.com>
2021-12-03 21:56:03 -0800

    Delete duplicate script

--- scripts/motivationSelection.js
+++ scripts/motivationSelection.js
@@ -1,7 +1,9 @@
+//this function takes a url and directs the webpage to the given url
 function goTo(pageURL) {
     window.location.href = pageURL
 }
 
+//this function accpets an element and a color as a string and changes the color of the element accordingly
 function changeBackgroundColor(element, color) {
     element.style.background = color;
 }
