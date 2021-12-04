4c3f97ea8e42ce573d9f7eb75ac2d2e67c894638
Prince H Chabveka <pchabveka@my.bcit.ca>
2021-12-03 18:34:37 -0800

    Change relative path

--- scripts/weather.js
+++ scripts/weather.js
@@ -27,19 +27,19 @@ button.addEventListener('click', function() {
             if (tempValue < 10) {
                 extra.innerHTML = "Seems too cold 🥶 in " + nameValue + ". Stay warm"
                 extra.style.color = "blue"
-                weatherpicture.innerHTML = "<img src = 'images/coldweather.jpg'/>"
+                weatherpicture.innerHTML = "<img src = '/images/coldweather.jpg'/>"
 
 
             } else if (tempValue > 10 && tempValue < 30) {
                 extra.innerHTML = "Could be warm enough in " + nameValue + ", just drink enough water!"
                 extra.style.color = "orange"
 
-                weatherpicture.innerHTML = "<img src = 'images/hotweather.jpg'/>"
+                weatherpicture.innerHTML = "<img src = '/images/hotweather.jpg'/>"
 
 
             } else {
                 extra.innerHTML = "Too hot for the outdoors!"
-                weatherpicture.innerHTML = "<img src = 'images/hotweatherwarning.jpeg'/>"
+                weatherpicture.innerHTML = "<img src = '/images/hotweatherwarning.jpeg'/>"
 
 
             }
@@ -52,89 +52,3 @@ button.addEventListener('click', function() {
 
 
 })
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const express = require("express");
-// const https = require('https');
-// const app = express();
-// const bodyParser = require('body-parser');
-// const { urlencoded } = require('body-parser');
-
-
-// app.use(express.urlencoded({ extended: true }))
-// app.use(express.urlencoded({ extended: true }))
-// app.use(express.static(__dirname + '/public'));
-// app.get("/", function(req, res) {
-
-//     res.sendFile(__dirname + "/weather.html");
-
-// })
-
-
-// app.post("/", function(req, res) {
-//     const units = 'metric'
-
-//     const query = req.body.cityName;
-//     const apikey = "f81e869ce68e56cb2fe9ed64be0a74b1"
-//     const url = "https://api.openweathermap.org/data/2.5/weather?q=" + query + "&appid=f81e869ce68e56cb2fe9ed64be0a74b1&units=" + units
-
-//     https.get(url, function(response) {
-
-//         response.on("data", function(data) {
-
-//             console.log(data);
-//             const weatherData = JSON.parse(data)
-//             const temp = weatherData.main.temp
-//             const weatherDescription = weatherData.weather[0].description
-//             const icon = weatherData.weather[0].icon
-//             const imageURL = "http://openweathermap.org/img/wn/" + icon + "@2x.png"
-//             console.log(weatherData)
-//             res.write("<p>Current weather: " + weatherDescription + "</p>");
-//             res.write("Current temp: " + temp + " degrees")
-//             res.write("<img src = " + imageURL + ">")
-
-//             res.send()
-
-//         })
-
-//     })
-
-// });
-
-
-
-// let port = 5500;
-// app.listen(port, function() {
-//     console.log('Prince application is on port ' + port + '!')
-// });
