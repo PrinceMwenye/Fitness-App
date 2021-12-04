ffff91c07a39653e03283354bc7a92182a73ac7c
Prince H Chabveka <pchabveka@my.bcit.ca>
2021-12-03 19:06:09 -0800

    Add recommended script

---
+++ scripts/recommended.js
@@ -0,0 +1,24 @@
+// <!-- Get recommended beach workout (random)  populate recommended workouts page with random workout which has been recommended -->
+
+
+db.collection("Reviews").where("Recommend", "==", "Yes")
+    .get()
+    .then(recom => {
+
+        size = recom.size;
+        recomendations = recom.docs;
+        var code = recomendations[0].data()['code'];
+        // console.log(code)
+
+        function read_recommended() {
+
+            db.collection("Workouts").doc("Beach workouts")
+                .onSnapshot(RecommendedDoc => {
+                    console.log(RecommendedDoc.data()[code])
+                    document.getElementById("recommended").src = RecommendedDoc.data()[code]
+                })
+        }
+
+        read_recommended()
+
+    })
