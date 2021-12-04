62e3c5eb99523fc7a70a93e37451fe8893872148
Prince H Chabveka <pchabveka@my.bcit.ca>
2021-12-03 19:02:57 -0800

    Add indoor script to specific folder

--- scripts/location.js
+++ scripts/location.js
@@ -42,3 +42,49 @@ function setWorkoutData(id) {
 read_display_beach_workout_one();
 read_display_beach_workout_two();
 read_display_beach_workout_three();
+
+
+
+
+// indoor  workouts
+function read_display_home_workout_one() {
+    db.collection("Workouts").doc("Indoor Workouts") //name of the collection and documents should matach excatly with what you have in Firestore
+        .onSnapshot(HomeWork => { //arrow notation
+            console.log("current document data: " + HomeWork.data().homeone); //.data() returns data object
+            document.getElementById("homeone").src = HomeWork.data().homeone; //using javascript to display the data on the right place
+
+        })
+}
+
+function read_display_home_workout_two() {
+    db.collection("Workouts").doc("Indoor Workouts") //name of the collection and documents should matach excatly with what you have in Firestore
+        .onSnapshot(HomeWork => { //arrow notation
+            // console.log("current document data: " + BeachWork.data().trailtwo); //.data() returns data object
+            document.getElementById("hometwo").src = HomeWork.data().hometwo; //using javascript to display the data on the right place
+
+        })
+}
+
+function read_display_home_workout_three() {
+    db.collection("Workouts").doc("Indoor Workouts") //name of the collection and documents should matach excatly with what you have in Firestore
+        .onSnapshot(HomeWork => { //arrow notation
+            // console.log("current document data: " + BeachWork.data().trailthree); //.data() returns data object
+            document.getElementById("homethree").src = HomeWork.data().homethree; //using javascript to display the data on the right place
+
+        })
+}
+
+
+// Add workout data in the local storage
+
+function setWorkoutData(id) {
+    localStorage.setItem('WorkoutID', id);
+}
+
+
+
+
+
+read_display_home_workout_one();
+read_display_home_workout_two();
+read_display_home_workout_three();
