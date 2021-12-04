ef9399519b8bbd8fda7b814366e84f50638c1dbe
OceNick <ozcenick@gmail.com>
2021-12-03 16:27:25 -0800

    Rename files

--- scripts/selection.js
+++
@@ -1,7 +0,0 @@
-   function goTo(pageURL) {
-                window.location.href = pageURL
-            }
-
-            function changeBackgroundColor(element, color) {
-                element.style.background = color;
-            }
